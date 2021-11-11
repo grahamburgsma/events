19725ecc35642cf74df9b9092494085260fff4ad
Graham Burgsma <graham.burgsma@gmail.com>
2021-11-10 21:00:14 -0500

    Fix missing default implementation for shouldQueue on AsyncListener

--- Sources/Events/Listener+Async.swift
+++ Sources/Events/Listener+Async.swift
@@ -2,24 +2,31 @@ import Vapor
 
 #if compiler(>=5.5) && canImport(_Concurrency)
 
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 public protocol AsyncListener: _Listener {
     associatedtype EventType: Event
+    
+    func shouldQueue(_ event: EventType, context: ListenerContext) -> Bool
 
-    @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
     func handle(_ event: EventType, context: ListenerContext) async throws
 }
 
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension AsyncListener {
     public func shouldQueue(_ event: EventType, context: ListenerContext) -> Bool { true }
 }
 
-extension AsyncListener {
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+extension _Listener where Self: AsyncListener {
     
-    @available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
-    public func _handle(_ event: EventType, context: ListenerContext) -> EventLoopFuture<Void> {
+    public func _shouldQueue<E>(_ event: E, context: ListenerContext) -> Bool where E : Event {
+        shouldQueue(event as! Self.EventType, context: context)
+    }
+    
+    public func _handle<E>(_ event: E, context: ListenerContext) -> EventLoopFuture<Void> where E : Event {
         let promise = context.eventLoop.makePromise(of: Void.self)
         promise.completeWithTask {
-            try await self.handle(event, context: context)
+            try await self.handle(event as! Self.EventType, context: context)
         }
         return promise.futureResult
     }
